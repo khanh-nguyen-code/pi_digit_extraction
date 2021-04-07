c504ff480bf0abf488301d94268c2f08172e0329
Nguyen Ngoc Khanh <nguyenngockhanh.pbc@gmail.com>
2021-04-08 01:41:20 +0800

    added main

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from pi_digit_extraction import nth_digit_hex, hex2dec
+from pi_digit_extraction import pi_digit, hex2dec
 import multiprocessing as mp
 int2hex = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"]
 pool = mp.Pool()
 N = 10000
-hex_list = pool.map(nth_digit_hex, range(N))
+hex_list = pool.map(pi_digit, range(N))
 
 # take N digits
 print("https://github.com/khanhhhh/pi_digit_extraction")
