b0d2e62d10b10733e1c41acf96f0f1895b99b48e
Nguyen Ngoc Khanh <nguyenngockhanh.pbc@gmail.com>
2021-04-08 03:43:55 +0800

    removed numpy, reformatted

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 if __name__ == "__main__":
     from pi_digit_extraction import pi_digit, hex2dec
     import multiprocessing as mp
-    import numpy as np
+    import random
+
     int2hex = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"]
     pool = mp.Pool()
-    N = 10000
+    N = 1000
     index_list = list(range(N))
-    np.random.shuffle(index_list)
-    argsort = np.argsort(index_list)
+    random.shuffle(index_list)
     hex_list = pool.map(pi_digit, index_list)
-    hex_list = list(np.array(hex_list)[argsort])
+    hex_list = [v for i, v in sorted(zip(index_list, hex_list))]
     # take N digits
     print("https://github.com/khanhhhh/pi_digit_extraction")
     print("3.", end="", flush=True)
