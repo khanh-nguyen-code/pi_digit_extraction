65d1807371afe4901b1e6eb2e6351633f146c9b7
Khanh Nguyen <khanh.nguyen@shopee.com>
2021-04-09 05:28:13 +0800

    fixed

--- digit_extraction/digit_extraction.go
+++ digit_extraction/digit_extraction.go
@@ -5,19 +5,32 @@ import (
 	"math/big"
 )
 
-// frac : return the fractional part of a/b
-func frac(a *big.Int, b *big.Int) float64 {
+// we treat float64 as fixed point of 53 bits
+var floatUnit = 53
+
+// ratFrac : return the fractional part of a/b for a > 0 and b > 0
+func ratFrac(a *big.Int, b *big.Int) float64 {
+	// make a < b
 	a = a.Mod(a, b)
+	// if bit length of a and b larger than 53, shift right until they have at most 53 in order to convert into float64
 	bitLen := b.BitLen()
-	if bitLen > 53 {
-		b = b.Rsh(b, uint(bitLen-53))
-		a = a.Rsh(a, uint(bitLen-53))
+	if bitLen > floatUnit {
+		b = b.Rsh(b, uint(bitLen-floatUnit))
+		a = a.Rsh(a, uint(bitLen-floatUnit))
 	}
 	return float64(a.Int64()) / float64(b.Int64())
 }
 
-// sumX : the fractional part of
-//        16^n s_x = 16^n \sum_{k=0}^{\infty} \frac{1}{16^k (8k+x)}
+// floatFrac : return the fractional part of x
+// 1.3 -> 0.3, -1.3 -> 0.7
+func floatFrac(x float64) float64 {
+	_, f := math.Modf(x)
+	if f < 0 {
+		f += 1
+	}
+	return f
+}
+
 var sixteen = big.NewInt(16)
 var eight = big.NewInt(8)
 var one = big.NewInt(1)
@@ -25,6 +38,8 @@ var four = big.NewInt(4)
 var five = big.NewInt(5)
 var six = big.NewInt(6)
 
+// sumX : the fractional part of
+//        16^n s_x = 16^n \sum_{k=0}^{\infty} \frac{1}{16^k (8k+x)}
 func sumX(n *big.Int, x *big.Int) float64 {
 	// finite sum
 	var k = big.NewInt(0)
@@ -32,10 +47,8 @@ func sumX(n *big.Int, x *big.Int) float64 {
 	for k.Cmp(n) != +1 {
 		denominator := (&big.Int{}).Add((&big.Int{}).Mul(eight, k), x)
 		numerator := (&big.Int{}).Exp(sixteen, (&big.Int{}).Sub(n, k), denominator)
-		s1 += frac(numerator, denominator)
-		if s1 >= 1 {
-			s1 -= 1
-		}
+		s1 += ratFrac(numerator, denominator)
+		s1 = floatFrac(s1)
 		k = k.Add(k, one)
 	}
 	// infinite sum
@@ -44,21 +57,18 @@ func sumX(n *big.Int, x *big.Int) float64 {
 		denominator := (&big.Int{}).Add((&big.Int{}).Mul(eight, k), x)
 		numeratorInv := (&big.Int{}).Exp(sixteen, (&big.Int{}).Sub(k, n), nil)
 		denominator = denominator.Mul(denominator, numeratorInv)
-		if denominator.BitLen() > 53 {
+		if denominator.BitLen() > floatUnit {
 			break
 		}
 		s2 += 1 / float64(denominator.Int64())
 		k = k.Add(k, one)
 	}
-	var s = s1 + s2
-	if s >= 1 {
-		s -= 1
-	}
-	return s
+	return floatFrac(s1 + s2)
 }
+
+// PiBPP : return n-th digit of PI in hexadecimal
 func PiBPP(n *big.Int) int {
-	var frac = 4*sumX(n, one) - 2*sumX(n, four) - sumX(n, five) - sumX(n, six)
-	frac -= math.Floor(frac)
+	var frac = floatFrac(4*sumX(n, one) - 2*sumX(n, four) - sumX(n, five) - sumX(n, six))
 	var digit = int(16 * frac)
 	return digit
 }
