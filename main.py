7a4b570f8082fa0b3d692fcf0d4ae9ce3bcb6f5b
khanh.nguyen <khanh.nguyen@shopee-ds-ip-10-128-148-194.idata-server.shopee.io>
2021-04-08 15:20:18 +0800

    added pool.close()

--- main.py
+++ main.py
@@ -55,12 +55,12 @@ def hex2dec(hex_list: List[int]) -> Iterator[int]:
 
 
 if __name__ == "__main__":
-    int2hex = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"]
     pool = mp.Pool()
     N = 1000
     index_list = list(range(N))
     random.shuffle(index_list)
     hex_list = pool.map(pi_digit, index_list)
+    pool.close()
     hex_list = [v for i, v in sorted(zip(index_list, hex_list))]
     # take N digits
     print("https://github.com/khanhhhh/pi_digit_extraction")
